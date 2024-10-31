5bcea0382a96057f004442c0b774f5f7bd5b5aa7
Кирилл Алаторцев <alatortsev.kv@phystech.su>
2024-10-31 16:27:20 +0300

    1st task complete

---
+++ task1/task1.cpp
@@ -0,0 +1,39 @@
+﻿// task1.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include <vector>
+
+
+
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	bool iter = 1; // возможно ли заполнение чана
+	int first, k_prev;
+	std::cin >> first;
+
+	k_prev = first;
+
+	for (std::size_t i = 1; i < n; i++) {
+		int k;
+		std::cin >> k;
+		if(k < k_prev){ 
+			k_prev = k;
+			iter = 0;
+			break;
+		}
+		k_prev = k;
+
+	}
+
+	if (iter) { std::cout << k_prev - first; }
+	else { std::cout << -1; }
+
+
+
+	;
+	return 0;
+}
